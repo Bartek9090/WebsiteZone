960ecf5312df91c03ffd005a72c7f3fa871745c7
Bartek Bartosik <b.bartek987@gmail.com>
2024-07-02 19:00:16 +0200

    fixing a header width

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
+const hamburgerToggle = document.getElementById("hamburger-toggle");
+const nav = document.querySelector(".nav");
+
+hamburgerToggle.addEventListener("change", function () {
+  if (this.checked) {
+    nav.classList.add("open");
+  } else {
+    nav.classList.remove("open");
+  }
+});
+
 const navLinks = document.querySelectorAll(".nav a");
 
 navLinks.forEach((link) => {
   link.addEventListener("click", () => {
-    const hamburgerToggle = document.getElementById("hamburger-toggle");
-    if (hamburgerToggle.checked) {
-      hamburgerToggle.checked = false;
-    }
+    hamburgerToggle.checked = false;
+    nav.classList.remove("open");
   });
 });
