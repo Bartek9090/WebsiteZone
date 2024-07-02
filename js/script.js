c6e3f05c50894811cf14febfada348d92887ccb7
Bartek Bartosik <b.bartek987@gmail.com>
2024-07-02 22:18:48 +0200

    fixing a header widt64

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
+// const hamburgerToggle = document.getElementById("hamburger-toggle");
+// const nav = document.querySelector(".nav");
+
+// hamburgerToggle.addEventListener("change", function () {
+//   if (this.checked) {
+//     nav.classList.add("open");
+//   } else {
+//     nav.classList.remove("open");
+//   }
+// });
+
+// const navLinks = document.querySelectorAll(".nav a");
+
+// navLinks.forEach((link) => {
+//   link.addEventListener("click", () => {
+//     hamburgerToggle.checked = false;
+//     nav.classList.remove("open");
+//   });
+// });
+
 const hamburgerToggle = document.getElementById("hamburger-toggle");
 const nav = document.querySelector(".nav");
 
 hamburgerToggle.addEventListener("change", function () {
   if (this.checked) {
-    nav.classList.add("open");
+    nav.style.display = "flex"; // Ustawienie display na flex, gdy checkbox jest zaznaczony
   } else {
-    nav.classList.remove("open");
+    nav.style.display = "none"; // Ustawienie display na none, gdy checkbox nie jest zaznaczony
   }
-});
-
-const navLinks = document.querySelectorAll(".nav a");
-
-navLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    hamburgerToggle.checked = false;
-    nav.classList.remove("open");
-  });
 });
