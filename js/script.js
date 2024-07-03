3e55de1ffae299b258c69d80d89b926e2915445d
Bartek Bartosik <b.bartek987@gmail.com>
2024-07-03 13:31:57 +0200

    fixing a header widt8

--- js/script.js
+++ js/script.js
@@ -1,30 +1,29 @@
-// const hamburgerToggle = document.getElementById("hamburger-toggle");
-// const nav = document.querySelector(".nav");
-
-// hamburgerToggle.addEventListener("change", function () {
-//   if (this.checked) {
-//     nav.classList.add("open");
-//   } else {
-//     nav.classList.remove("open");
-//   }
-// });
-
-// const navLinks = document.querySelectorAll(".nav a");
-
-// navLinks.forEach((link) => {
-//   link.addEventListener("click", () => {
-//     hamburgerToggle.checked = false;
-//     nav.classList.remove("open");
-//   });
-// });
-
 const hamburgerToggle = document.getElementById("hamburger-toggle");
 const nav = document.querySelector(".nav");
+const navLinks = document.querySelectorAll(".nav__link");
 
 hamburgerToggle.addEventListener("change", function () {
   if (this.checked) {
-    nav.style.display = "flex"; // Ustawienie display na flex, gdy checkbox jest zaznaczony
+    openNav();
   } else {
-    nav.style.display = "none"; // Ustawienie display na none, gdy checkbox nie jest zaznaczony
+    closeNav();
   }
 });
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    closeNav();
+  });
+});
+
+function openNav() {
+  if (window.innerWidth <= 767) {
+    nav.style.display = "flex";
+  }
+}
+
+function closeNav() {
+  if (window.innerWidth <= 767) {
+    nav.style.display = "none";
+  }
+}
