ddc8211cade86f4d821d720a5b088867235768c5
Bartek Bartosik <b.bartek987@gmail.com>
2024-07-03 13:48:07 +0200

    fixing a header widt9

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 const hamburgerToggle = document.getElementById("hamburger-toggle");
 const nav = document.querySelector(".nav");
 const navLinks = document.querySelectorAll(".nav__link");
+const hamburgerLabel = document.querySelector(".hamburger");
 
 hamburgerToggle.addEventListener("change", function () {
   if (this.checked) {
     openNav();
   } else {
     closeNav();
+  }
+});
+
+hamburgerLabel.addEventListener("click", function () {
+  if (hamburgerToggle.checked) {
+    closeNav();
+  } else {
+    openNav();
   }
 });
 
@@ -25,5 +34,6 @@ function openNav() {
 function closeNav() {
   if (window.innerWidth <= 767) {
     nav.style.display = "none";
+    hamburgerToggle.checked = false; // Odznaczenie checkboxa
   }
 }
